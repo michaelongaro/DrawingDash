54ad9afee4632c97c61d45b7718d980b67d88892
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-08-23 04:48:07 -0500

    fixed bug where newest seen pfp updated your own pfp

--- src/components/layout/ProfilePicture.js
+++ src/components/layout/ProfilePicture.js
@@ -1,17 +1,43 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 
-import ProfilePictureUpdateContext from "./ProfilePictureUpdateContext";
+import {
+  getDatabase,
+  ref as ref_database,
+  child,
+  get,
+} from "firebase/database";
+
+import {
+  getDownloadURL,
+  getStorage,
+  ref as ref_storage,
+} from "firebase/storage";
+
+import { app } from "../../util/init-firebase";
 
 import classes from "./ProfilePicture.module.css";
 import baseClasses from "../../index.module.css";
 
 const ProfilePicture = ({ user, size }) => {
-  const PFPUpdateCtx = useContext(ProfilePictureUpdateContext);
+  const dbRef = ref_database(getDatabase(app));
+  const storage = getStorage();
 
   const [isFetching, setIsFetching] = useState(true);
+  const [showTempBaselineSkeleton, setShowTempBaselineSkeleton] =
+    useState(true);
+  const [imageElementLoaded, setImageElementLoaded] = useState(false);
 
+  const [image, setImage] = useState(null);
   const [shimmerStyle, setShimmerStyle] = useState(size);
   const [roundedProfileStyle, setRoundedProfileStyle] = useState(size);
+
+  useEffect(() => {
+    const timerID = setTimeout(() => setShowTempBaselineSkeleton(false), 500);
+
+    return () => {
+      clearTimeout(timerID);
+    };
+  }, []);
 
   useEffect(() => {
     if (size === "small") {
@@ -25,20 +51,40 @@ const ProfilePicture = ({ user, size }) => {
       setRoundedProfileStyle(classes.roundedProfileLarge);
     }
 
-    PFPUpdateCtx.fetchProfilePicture(user);
+    getDownloadURL(ref_storage(storage, `users/${user}/croppedProfile`))
+      .then((url) => {
+        setImage(url);
+      })
+      .catch((error) => {
+        if (
+          error.code === "storage/object-not-found" ||
+          error.code === "storage/unknown"
+        ) {
+          // defaulting to auth0 image
+          get(child(dbRef, "dailyPrompts")).then((snapshot) => {
+            if (snapshot.exists()) {
+              setImage(snapshot.val()["defaultProfilePicture"]);
+            }
+          });
+        }
+      });
   }, []);
 
   useEffect(() => {
-    if (PFPUpdateCtx.image !== null) {
+    if (image !== null) {
       setIsFetching(false);
     }
-  }, [PFPUpdateCtx.image]);
+  }, [image]);
 
   return (
     <>
       {isFetching ? (
         <div
           style={{
+            display:
+              isFetching || showTempBaselineSkeleton || !imageElementLoaded
+                ? "block"
+                : "none",
             width: size === "small" ? "50px" : "65px",
             height: size === "small" ? "50px" : "65px",
             borderRadius: "50%",
@@ -48,9 +94,18 @@ const ProfilePicture = ({ user, size }) => {
       ) : (
         <div className={shimmerStyle}>
           <img
+            style={{
+              display:
+                !isFetching && !showTempBaselineSkeleton && imageElementLoaded
+                  ? "block"
+                  : "none",
+            }}
             className={roundedProfileStyle}
-            src={PFPUpdateCtx.image}
+            src={image}
             alt="Profile"
+            onLoad={() => {
+              setImageElementLoaded(true);
+            }}
           />
         </div>
       )}
